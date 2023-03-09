30a028283fc86e10ccb29f1d8b69e8c035ea410b
bharu222 <127390161+bharu222@users.noreply.github.com>
2023-03-09 14:29:20 +0530

    Add files via upload
    added

---
+++ addition.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class subraction
+{
+    public static void main(String[] args)
+    {
+      
+       int num1=10,num2=20,sub=0;
+
+       System.out.println("num1 = "+num1);
+       System.out.println("num2 = "+num2);
+       sub=num1-num2;
+       System.out.println("The sub = "+sub);
+     }
+}
